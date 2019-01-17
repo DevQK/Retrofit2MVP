f6bc664c9942f9b24897f8889918daca9b03705f
C:\Program Files\Git\cmd\git.exe <devdqkhanh@gmail.com>
2019-01-18 00:17:55 +0700

    Main

--- app/src/main/java/khanh/retrofit2mvp/MainActivity.java
+++ app/src/main/java/khanh/retrofit2mvp/MainActivity.java
@@ -2,6 +2,7 @@ package khanh.retrofit2mvp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toast.makeText(this, "DEVQK", Toast.LENGTH_SHORT).show();
     }
 }
