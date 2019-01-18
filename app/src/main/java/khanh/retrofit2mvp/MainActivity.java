528ea8d9b2eadeb3ab3cb3ee9ca4d8458fc3b27e
C:\Program Files\Git\cmd\git.exe <devdqkhanh@gmail.com>
2019-01-18 22:33:19 +0700

    upload

--- app/src/main/java/khanh/retrofit2mvp/MainActivity.java
+++ app/src/main/java/khanh/retrofit2mvp/MainActivity.java
@@ -2,29 +2,16 @@ package khanh.retrofit2mvp;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.RecyclerView;
-
-import butterknife.BindView;
-import butterknife.ButterKnife;
-import butterknife.OnClick;
-import khanh.retrofit2mvp.Presenter.ProductsPresenter;
 
 public class MainActivity extends AppCompatActivity {
 
-    @BindView(R.id.recyclerView)
-    RecyclerView recyclerView;
-    private ProductsPresenter productsPresenter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ButterKnife.bind(this);
-        productsPresenter = new ProductsPresenter(this, recyclerView);
-    }
-    @OnClick(R.id.btnGetData)
-    public void getData() {
-        productsPresenter.fetchData();
 
     }
+
 }
--- app/src/main/java/khanh/retrofit2mvp/Model/API/Constant.java
+++
@@ -1,5 +0,0 @@
-package khanh.retrofit2mvp.Model.API;
-
-public class Constant {
-    public static final String URLANDROID = "http://dev.androidcoban.com/blog/";
-}
