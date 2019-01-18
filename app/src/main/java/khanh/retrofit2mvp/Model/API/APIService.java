92506d3987c3133037b5ece9707ebaf9146bc65d
C:\Program Files\Git\cmd\git.exe <devdqkhanh@gmail.com>
2019-01-18 17:17:54 +0700

    API Service

--- app/src/main/java/khanh/retrofit2mvp/Model/API/APIService.java
+++ app/src/main/java/khanh/retrofit2mvp/Model/API/APIService.java
@@ -5,7 +5,7 @@ import retrofit2.Call;
 import retrofit2.http.GET;
 
 public interface APIService {
-    @GET('getallproducts.php')
+    @GET("getallproducts.php")
     Call<ResponseBody> getAllProduct();
-    
+
 }
