a939f7f6495c5b51d02196f3c5bc66f4db255cdf
C:\Program Files\Git\cmd\git.exe <devdqkhanh@gmail.com>
2019-01-18 17:57:03 +0700

    Products presenter extends base presenter

--- app/src/main/java/khanh/retrofit2mvp/Presenter/ProductsPresenter.java
+++ app/src/main/java/khanh/retrofit2mvp/Presenter/ProductsPresenter.java
@@ -1,4 +1,15 @@
 package khanh.retrofit2mvp.Presenter;
 
-public class ProductsPresenter {
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+
+public class ProductsPresenter extends BasePresenter{
+
+    String TAG = ProductsPresenter.class.getSimpleName();
+    private RecyclerView recyclerView;
+
+
+    public ProductsPresenter(Context ct) {
+        super(ct);
+    }
 }
