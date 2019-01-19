a48f3acdab591c66a313edb9269647742bbdce7d
C:\Program Files\Git\cmd\git.exe <devdqkhanh@gmail.com>
2019-01-19 14:52:05 +0700

    View Scopes

---
+++ app/src/main/java/khanh/retrofit2mvp/View/Scopes/ActivityScopes.java
@@ -0,0 +1,9 @@
+package khanh.retrofit2mvp.View.Scopes;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import javax.inject.Scope;
+@Scope
+@Retention(RetentionPolicy.CLASS)
+public @interface ActivityScopes {
+}
---
+++ app/src/main/java/khanh/retrofit2mvp/View/Scopes/ApplicationScopes.java
@@ -0,0 +1,11 @@
+package khanh.retrofit2mvp.View.Scopes;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+import javax.inject.Scope;
+
+@Scope
+@Retention(RetentionPolicy.CLASS)
+public @interface ApplicationScopes {
+}
