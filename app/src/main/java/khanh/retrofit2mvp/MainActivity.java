97472d10c975ae3e0338159fafe68e8011d91bb1
C:\Program Files\Git\cmd\git.exe <devdqkhanh@gmail.com>
2019-01-19 14:42:13 +0700

    Main

--- app/src/main/java/khanh/retrofit2mvp/MainActivity.java
+++ app/src/main/java/khanh/retrofit2mvp/MainActivity.java
@@ -1,9 +1,51 @@
 package khanh.retrofit2mvp;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.List;
+
+import javax.inject.Inject;
+
+import khanh.retrofit2mvp.Model.CryptoData;
+import khanh.retrofit2mvp.Model.MainActivityContextModule;
+import khanh.retrofit2mvp.Model.MainActivityMvpModule;
+import khanh.retrofit2mvp.Presenter.PresenterImpl;
+import khanh.retrofit2mvp.View.Component.ApplicationComponent;
+import khanh.retrofit2mvp.View.Component.DaggerMainActivityComponent;
+import khanh.retrofit2mvp.View.Component.MainActivityComponent;
+import khanh.retrofit2mvp.View.Contract.MainActivityContract;
+import khanh.retrofit2mvp.View.MainView.MyApplication;
+import khanh.retrofit2mvp.View.MainView.RecyclerViewAdapter;
+import khanh.retrofit2mvp.View.Qualifier.ActivityContent;
+import khanh.retrofit2mvp.View.Qualifier.ApplicationContent;
+
+
+public class MainActivity extends AppCompatActivity implements MainActivityContract.View, RecyclerViewAdapter.ClickListener{
+    private RecyclerView recyclerView;
+    private ProgressBar progressBar;
+    MainActivityComponent mainActivityComponent;
+
+    @Inject
+    public RecyclerViewAdapter recyclerViewAdapter;
+
+
+    @Inject
+    @ApplicationContent
+    public Context mContext;
+
+    @Inject
+    @ActivityContent
+    public Context activityContext;
+
+    @Inject
+    PresenterImpl presenter;
 
 
     @Override
@@ -11,6 +53,50 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ApplicationComponent applicationComponent = MyApplication.get(this).getApplicationComponent();
+        mainActivityComponent = DaggerMainActivityComponent.builder()
+                .mainActivityContextModule(new MainActivityContextModule(this))
+                .mainActivityMvpModule(new MainActivityMvpModule(this))
+                .applicationComponent(applicationComponent)
+                .build();
+
+        mainActivityComponent.injectMainActivity(this);
+
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(activityContext));
+        recyclerView.setAdapter(recyclerViewAdapter);
+        progressBar = findViewById(R.id.progressBar);
+
+        presenter.loadData();
+    }
+    @Override
+    public void launchIntent(String name) {
+        Toast.makeText(mContext, name, Toast.LENGTH_SHORT).show();
+        // startActivity(new Intent(activityContext, DetailActivity.class).putExtra("name", name));
     }
 
+    @Override
+    public void showData(List<CryptoData> data) {
+        recyclerViewAdapter.setData(data);
+    }
+
+    @Override
+    public void showError(String message) {
+        Toast.makeText(mContext, message, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void showComplete() {
+
+    }
+
+    @Override
+    public void showProgress() {
+        progressBar.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    public void hideProgress() {
+        progressBar.setVisibility(View.GONE);
+    }
 }
